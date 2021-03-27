3499ff6c7e9dc2eb04436527fba1c9ee1dc08925
Koen <koen@bravemail.nl>
2021-03-27 19:53:38 +0100

    Add files via upload

--- grafiek.py
+++ grafiek.py
@@ -6,38 +6,14 @@ from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-import numpy
+import numpy as np
+import pandas as pd 
+
+from keras.preprocessing import image
 import pickle
 
+model = keras.models.load_model('./trained_model_64_one')
 
-rock = os.path.join("./Rock-Paper-Scissors/train/rock/")
-paper = os.path.join("./Rock-Paper-Scissors/train/paper/")
-scissors = os.path.join("./Rock-Paper-Scissors/train/scissors/")
-
-print("Rock : ",len(os.listdir(rock)))
-print("Paper : ",len(os.listdir(rock)))
-print("Scissors : ",len(os.listdir(rock)))
-
-rockFiles = os.listdir(rock)
-paperFiles = os.listdir(paper)
-scissorsFiles = os.listdir(scissors)
-
-model = keras.Sequential([
-     keras.layers.Conv2D(32, (3,3), activation="relu", input_shape=(150,150,3)),
-     keras.layers.MaxPooling2D(2,2),
-    
-    keras.layers.Conv2D(64, (3,3), activation="relu"),
-    keras.layers.MaxPooling2D(2,2),
-
-    
-    keras.layers.Flatten(),
-    keras.layers.Dropout(0.6),
-    keras.layers.Dense(512, activation="relu"),
-    keras.layers.Dense(3, activation="softmax")
-])
-
-model.summary()
-model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
 
 train_datagen = ImageDataGenerator(rescale=1./255,
                                   rotation_range=40,
@@ -60,14 +36,37 @@ validation_generator = validation_datagen.flow_from_directory("./Rock-Paper-Scis
                                                              class_mode="categorical")
 
 history = model.fit(train_generator, validation_data=validation_generator,
-                   validation_steps=3,epochs=20,
-                   steps_per_epoch=20,
+                   validation_steps=1,epochs=5,
+                   steps_per_epoch=1,
                    verbose=1)
 
-name = "Model_test_8_" + str(history.history["accuracy"][-1])
+
+
+name = "Model_512_" + str(history.history["accuracy"][-1])
 
 model.save(name)
 name = name + "_history"
 
 with open(name, 'wb') as file_pi:
         pickle.dump(history.history, file_pi)
+
+acc = history.history["accuracy"]
+val_acc = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+
+epoch = range(len(acc))
+
+plt.plot(epoch, acc, "r", label = "Training Accuracy")
+plt.plot(epoch, val_acc, "b", label = "Validation Accuracy")
+plt.title("Training And Validation Accuracy")
+
+plt.legend()
+plt.figure()
+
+plt.plot(epoch, loss, "r", label = "Training Loss")
+plt.plot(epoch, val_loss, "b", label = "Validation Loss")
+plt.title("Training And Validation Loss")
+
+plt.legend()
+plt.show()
